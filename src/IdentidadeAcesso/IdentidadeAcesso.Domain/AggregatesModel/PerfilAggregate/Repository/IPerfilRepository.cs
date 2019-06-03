bae655e6c37945cc7fe4f67b2ea51a9dfbee6809
Maycon Rayone <maycon.rayone@gmail.com>
2019-06-03 12:39:01 -0300

    * Criando a implementação de IRepository. * Criando interfaces de repositório nas ARs.

---
+++ src/IdentidadeAcesso/IdentidadeAcesso.Domain/AggregatesModel/PerfilAggregate/Repository/IPerfilRepository.cs
@@ -0,0 +1,12 @@
+﻿using IdentidadeAcesso.Domain.SeedOfWork.interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IdentidadeAcesso.Domain.AggregatesModel.PerfilAggregate.Repository
+{
+    public interface IPerfilRepository : IRepository<Perfil>
+    {
+
+    }
+}
---
+++ src/IdentidadeAcesso/IdentidadeAcesso.Domain/SeedOfWork/interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace IdentidadeAcesso.Domain.SeedOfWork.interfaces
+{
+    public interface IRepository<TEntity> : IDisposable where TEntity : Entity, IAggregateRoot
+    {
+        void Adicionar(TEntity obj);
+        TEntity ObterPorId(Guid id);
+        IEnumerable<TEntity> ObterTodos();
+        void Atualizar(TEntity obj);
+        void Remover(Guid id);
+        IEnumerable<TEntity> Buscar(Expression<Func<TEntity, bool>> predicate);
+        int SaveChanges();
+    }
+}
