a475dbfdded5814a4d88a20c0b2d0196c6a751bf
Maycon Rayone <maycon.rayone@gmail.com>
2019-06-04 15:45:19 -0300

    instalação de dependências e criação do contexto.

---
+++ src/Knowledge.IO.Infra.Data/Context/IdentidadeAcessoContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Knowledge.IO.Infra.Data.Context
+{
+    public class IdentidadeAcessoContext : DbContext
+    {
+    }
+}
