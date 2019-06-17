7310036fec7ecabe5e77cef704730f372e131b7b
Maycon Rayone <maycon.rayone@gmail.com>
2019-06-17 11:33:49 -0300

    modificando validation error.

--- src/IdentidadeAcesso/IdentidadeAcesso.Domain/SeedOfWork/Validation/ValidationDomainResult.cs
+++ src/IdentidadeAcesso/IdentidadeAcesso.Domain/SeedOfWork/Validation/ValidationDomainResult.cs
@@ -37,6 +37,43 @@ namespace IdentidadeAcesso.Domain.SeedOfWork.Validation
 
         public string Property { get; private set; }
         public string MessageError { get; private set; }
-        
+
+        public override bool Equals(object other)
+        {
+            var compareTo = other as ValidationError;
+
+            if (ReferenceEquals(this, compareTo)) return true;
+            if (ReferenceEquals(null, compareTo)) return false;
+
+            return compareTo.Property == Property && compareTo.MessageError == MessageError;
+        }
+
+
+        public static bool operator ==(ValidationError a, ValidationError b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+                return true;
+
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(ValidationError a, ValidationError b)
+        {
+            return !(a == b);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = MessageError.GetHashCode();
+                hashCode += (Property.GetHashCode() * 907) ^ Property.GetHashCode();
+
+                return hashCode;
+            }
+        }
     }
 }
