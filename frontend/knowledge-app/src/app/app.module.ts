c6cd399b6c3d00ae073e1adcefcc23f30da0b707
Maycon Rayone <maycon.rayone@gmail.com>
2019-07-02 11:54:55 -0300

    adicionando font e iniciando menu

--- frontend/knowledge-app/src/app/app.module.ts
+++ frontend/knowledge-app/src/app/app.module.ts
@@ -2,11 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
+//components
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
