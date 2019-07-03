7ac2e2f6658ac844df793b5604fe1d1d76efbdbc
Maycon Rayone <maycon.rayone@gmail.com>
2019-07-03 20:00:31 -0300

    link ativado.

--- frontend/knowledge-app/src/app/app-routing.module.ts
+++ frontend/knowledge-app/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PerfilComponent } from './components/perfil/perfil.component';
+import { DashBoardComponent } from './components/dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: 'perfil', component: PerfilComponent }
+  { path: 'perfil', component: PerfilComponent },
+  { path: 'dashboard', component: DashBoardComponent },
 ];
 
 @NgModule({
