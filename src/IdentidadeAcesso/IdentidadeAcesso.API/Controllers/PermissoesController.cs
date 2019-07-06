72fe40aa9af6ff2845f55627e22978ed98dd4484
Maycon Rayone <maycon.rayone@gmail.com>
2019-07-06 15:48:40 -0300

    criando interface de consulta e view model.

---
+++ src/IdentidadeAcesso/IdentidadeAcesso.API/Controllers/PermissoesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentidadeAcesso.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PermissoesController : ControllerBase
+    {
+        public PermissoesController()
+        {
+
+        }
+
+
+    }
+}
---
+++ tests/IdentidadeAcesso/IdentidadeAcesso.Services.Tests/ControllersTest/PermissaoControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IdentidadeAcesso.Services.UnitTests.ControllersTest
+{
+    public class PermissaoControllerTest
+    {
+        public PermissaoControllerTest()
+        {
+
+        }
+
+        [Fact(DisplayName = "Deve retornar uma lista de permissões")]
+        [Trait("Controller", "Permissão")]
+        public async Task Deve_Retornar_Lista_De_Permissoes()
+        {
+
+        }
+    }
+}
