fb2cbe4ac43b661c11eca8e10967e922f711c423
Maycon Rayone <maycon.rayone@gmail.com>
2019-07-29 11:24:04 -0300

    adicionando novas permissões ao super admin.

--- src/IdentidadeAcesso/IdentidadeAcesso.Infra.Data/Migrations/20190729141732_adicionando-novas-permissoes.Designer.cs
+++ src/IdentidadeAcesso/IdentidadeAcesso.Infra.Data/Migrations/20190729141732_adicionando-novas-permissoes.Designer.cs
@@ -4,14 +4,16 @@ using Knowledge.IO.Infra.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace IdentidadeAcesso.Infra.Data.Migrations
 {
     [DbContext(typeof(IdentidadeAcessoDbContext))]
-    partial class IdentidadeAcessoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190729141732_adicionando-novas-permissoes")]
+    partial class adicionandonovaspermissoes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
