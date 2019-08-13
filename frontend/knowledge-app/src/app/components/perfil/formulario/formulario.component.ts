072069fc35428ef86debbfca226b9436eaa215d2
Maycon Rayone <maycon.rayone@gmail.com>
2019-08-13 08:39:30 -0300

    configurado radiobuttons e estilos

--- frontend/knowledge-app/src/app/components/perfil/formulario/formulario.component.ts
+++ frontend/knowledge-app/src/app/components/perfil/formulario/formulario.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit, Input, EventEmitter, Output, ViewChildren, ElementRef, AfterViewInit } from '@angular/core';
 import { Perfil } from '../models/perfil';
 import { FormType } from 'src/app/Utils/formType/form-type.enum';
-import { FormGroup, FormControlName, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControlName, FormControl, Validators, FormArray } from '@angular/forms';
 import { GenericValidator } from 'src/app/Utils/generic-validator';
 import { mensagensDeErroPerfilForm } from './mensagens-de-erro/mensagens-de-erro';
 import { Observable, fromEvent, merge } from 'rxjs';
 import { PermissaoService } from 'src/app/services/permissao.service';
 import { Permissao } from '../../permissao/Models/permissao';
-
+import * as _ from "lodash";
 @Component({
   selector: 'app-formulario-perfil',
   templateUrl: './formulario.component.html',
@@ -16,42 +16,60 @@ import { Permissao } from '../../permissao/Models/permissao';
 export class FormularioComponent implements OnInit, AfterViewInit {
 
   perfilForm: FormGroup;
-  private permissoes: Permissao[];
-  @Input() model: Perfil;
-  @Input() formType: FormType = FormType.Post;
+  permissoes: Permissao[];
   erros: any = {};
   genericValidator: any;
-
+  @Input() model: Perfil;
+  @Input() formType: FormType = FormType.Post;
   @Output() command = new EventEmitter<FormGroup>();
   @ViewChildren(FormControlName, { read: ElementRef }) formInputElements: ElementRef[];
 
   constructor(private permissoesService: PermissaoService) {
     this.genericValidator = new GenericValidator(mensagensDeErroPerfilForm);
-    this.model =  new Perfil();
+    this.model = new Perfil();
     this.permissoes = new Array<Permissao>();
-   }
-
-  ngOnInit() {
-    this.gerarFormulario();
-
-    this.permissoesService.getTodas().subscribe(permissoes => {
-      this.permissoes = permissoes;
-    });
   }
 
+  ngOnInit() {
+    this.permissoesService.getTodas().subscribe(permissoes => {
+      this.permissoes = permissoes;
 
-  get mapPermissoes(): any {
-    const permissoes = [];
-    this.permissoes.forEach((value, index, array) => {
-      const valores = array.filter((el) => el.tipo === value.tipo);
-      if (permissoes.findIndex((p) => p.tipo === value.tipo) === -1) {
-        permissoes.push({
-          tipo: value.tipo,
-          valores
-        });
-      }
+      const array = [];
+      this.permissoes.forEach((el) => {
+        array.push(new FormControl());
+      });
+
+      this.perfilForm.addControl('atribuicoes', new FormArray(array));
     });
-    return permissoes;
+
+    this.gerarFormulario();
+  }
+
+  //TODO: o que interessa é apenas o valor e o id.
+  //talvez seja melhor uilizar apenas os valores e criar o array de radio buttons.
+  // get mapPermissoes(): any {
+  //   const permissoes = [];
+  //   this.permissoes.forEach((value, index, array) => {
+  //     const valores = array.filter((el) => el.tipo === value.tipo);
+  //     if (permissoes.findIndex((p) => p.tipo === value.tipo) === -1) {
+  //       this.perfilForm.addControl('atribuicoes', new FormArray([
+
+  //       ]));
+  //       permissoes.push({
+  //         tipo: value.tipo,
+  //         valores
+  //       });
+  //     }
+  //   });
+  //   return permissoes;
+  // }
+
+  radioAtivo(index, value): boolean {
+    const control = this.perfilForm.value.atribuicoes[index];
+    if (control) {
+      return control.ativo === value.ativo;
+    }
+    return false;
   }
 
   gerarFormulario() {
@@ -64,13 +82,18 @@ export class FormularioComponent implements OnInit, AfterViewInit {
       descricao: new FormControl(this.model.descricao, [
         Validators.minLength(3),
         Validators.maxLength(150)
-      ]),
+      ])
     });
 
     if (this.formType === FormType.Put) {
       this.perfilForm.addControl('id', new FormControl(this.model.id)
       );
     }
+  }
+
+  criarValor(permissaoId, ativo) {
+
+    return { permissaoId, ativo };
   }
 
   ngAfterViewInit(): void {
@@ -83,6 +106,8 @@ export class FormularioComponent implements OnInit, AfterViewInit {
   }
 
   sendCommand() {
-    this.command.emit(this.perfilForm);
+    //this.command.emit(this.perfilForm);
+    console.log(this.model);
   }
 }
+
