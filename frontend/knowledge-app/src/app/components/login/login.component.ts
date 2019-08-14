776ad73c2c80338f631fe870cf8caa69884963ed
Maycon Rayone <maycon.rayone@gmail.com>
2019-08-14 16:24:29 -0300

    adicionando inrequest check no btn de login.

--- frontend/knowledge-app/src/app/components/login/login.component.ts
+++ frontend/knowledge-app/src/app/components/login/login.component.ts
@@ -10,6 +10,7 @@ import { AppState } from 'src/app/store/reducers';
 import { ObterTokenModel } from 'src/app/store/selectors/app.selector';
 import { GenericValidator } from 'src/app/Utils/generic-validator';
 import { mensagensDeErro } from './mensgens-de-erro/mensagens';
+import { InrequestService } from 'src/app/services/inrequest.service';
 
 
 @Component({
@@ -19,13 +20,13 @@ import { mensagensDeErro } from './mensgens-de-erro/mensagens';
 })
 export class LoginComponent implements OnInit, AfterViewInit {
   loginForm: FormGroup;
-  inRequest$: Observable<boolean>;
 
   @ViewChildren(FormControlName, { read: ElementRef }) formInputElements: ElementRef[];
   genericValidator: GenericValidator;
   erros?: { [key: string]: string } = {};
 
-  constructor(private loginService: LogInService, private stateService: Store<AppState>, private router: Router) {
+  constructor(private loginService: LogInService, private stateService: Store<AppState>, public inRequestService: InrequestService,
+     private router: Router) {
     this.genericValidator = new GenericValidator(mensagensDeErro);
   }
 
