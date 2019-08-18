fbd6265981c91c245cc8f4cb931a0dcc65d1ab26
Maycon Rayone <maycon.rayone@gmail.com>
2019-08-18 15:02:00 -0300

    tornando menu responsivo.

--- frontend/knowledge-app/src/app/components/shared/menu/menu.component.ts
+++ frontend/knowledge-app/src/app/components/shared/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
@@ -7,11 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  isCollapseUsuario: false;
-  isCollapsePermissao: false;
-  isCollapsePerfil: false;
+  isCollapseUsuario = false;
+  isCollapsePermissao = false;
+  isCollapsePerfil = false;
+  @Input() showMenu = false;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
