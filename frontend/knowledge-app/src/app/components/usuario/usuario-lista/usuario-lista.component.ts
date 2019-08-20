d3136289e07e431de900ca6daa21a98fc8ef0ee0
Maycon Rayone <maycon.rayone@gmail.com>
2019-08-20 00:01:25 -0300

    adicionando data table.

--- frontend/knowledge-app/src/app/components/usuario/usuario-lista/usuario-lista.component.ts
+++ frontend/knowledge-app/src/app/components/usuario/usuario-lista/usuario-lista.component.ts
@@ -1,31 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { UsuarioService } from 'src/app/services/usuario.service';
-import { Usuario } from '../models/usuario';
+import { DataTableService } from '../../shared/data-table/interfaces/data-table-service';
+import { DecimalPipe } from '@angular/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-usuario-lista',
   templateUrl: './usuario-lista.component.html',
-  styleUrls: ['./usuario-lista.component.scss']
+  styleUrls: ['./usuario-lista.component.scss'],
+  providers: [DataTableService, DecimalPipe]
 })
 export class UsuarioListaComponent implements OnInit {
 
-  private getUsuarios: Usuario[];
+  getUsuarios$: Observable<any[]>;
   page = 1;
   pageSize = 4;
   collectionSize = 0;
-  constructor(private usuarioService: UsuarioService) {
-    this.getUsuarios = new Array<Usuario>();
+  constructor(private usuarioService: UsuarioService, private router: Router) {
+
   }
 
   ngOnInit() {
-    this.usuarioService.getTodos().subscribe(usuarios => {
-      this.getUsuarios = usuarios;
-      this.collectionSize = this.getUsuarios.length;
-    });
-  }
-  get usuarios(): Usuario[] {
-
-    return this.getUsuarios.map((usuario, i) => ({ id: i + 1, ...usuario }))
-      .slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize)
+    this.getUsuarios$ = this.usuarioService.getTodos().pipe(
+      map((usuarios) => {
+        usuarios.map((el) => {
+          el.action = {
+            view: this.router.createUrlTree(['usuarios/detalhes', el.id]).toString(),
+            edit: this.router.createUrlTree(['usuarios/editar', el.id]).toString(),
+            delete: this.router.createUrlTree(['usuarios/detalhes', el.id]).toString()
+          };
+        });
+        return usuarios;
+      })
+    );
   }
 }
+
