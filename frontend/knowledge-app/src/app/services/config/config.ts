3c7ab85f8e53de25f17368338e0d47cf2d353833
Maycon Rayone <maycon.rayone@gmail.com>
2019-08-24 11:15:27 -0300

    configurações do ambiente de prod e dev.

--- frontend/knowledge-app/src/app/services/config/config.ts
+++ frontend/knowledge-app/src/app/services/config/config.ts
@@ -1,6 +1,7 @@
 import { HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
-export const url = 'https://knowledgeback.azurewebsites.net';
+export const url = environment.apiUrl;
 
 export const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
